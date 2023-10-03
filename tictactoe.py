9b330ae05332a3fe8712665a9ceb2df9d2971719
duhboto <129698401+duhboto@users.noreply.github.com>
2023-10-03 16:59:04 -0700

    Add utility function in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -83,7 +83,13 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
