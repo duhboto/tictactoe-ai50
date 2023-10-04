543e43ce19253b8ccf90b868c599be68801d7043
duhboto <129698401+duhboto@users.noreply.github.com>
2023-10-03 17:10:25 -0700

    Add pruning to minimax  in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,9 @@
-"""
-Tic Tac Toe Player
-"""
-
 import math
+from copy import deepcopy
 
 X = "X"
 O = "O"
 EMPTY = None
-
 
 def initial_state():
     """
@@ -17,57 +13,54 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
-
-def get_next_player(board):
+def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    move_count = sum(cell == 'X' or cell == 'O' for row in board for cell in row)
-  
-    #(Assuming X always goes first). If move count is even, it is X's turn. If odd, it's O's turn.
-    return 'X' if move_count % 2 == 0 else 'O'
- 
- 
+    x_turns = sum([row.count(X) for row in board])
+    o_turns = sum([row.count(O) for row in board])
 
+    return X if x_turns <= o_turns else O
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    return {(i, j) for i in range(3) for j in range(3) if board[i][j] == EMPTY}
-
+    return [(i, j) for i in range(3) for j in range(3) if board[i][j] == EMPTY]
 
 def result(board, action):
     """
     Returns the board that results from making a move, action, on the board.
     """
     i, j = action
-    player = get_next_player(board)
-    if board[i][j] == EMPTY:
-        new_board = create_new_board(board)
-        new_board[i][j] = player
-        return new_board
-    else:
+    if board[i][j] != EMPTY:
         raise Exception("Invalid action.")
 
+    new_board = deepcopy(board)
+    new_board[i][j] = player(board)
+
+    return new_board
 
 
 def winner(board):
     lines = [
-        [board[i][0] for i in range(3)],  # cols
-        [board[i][1] for i in range(3)],  # cols
-        [board[i][2] for i in range(3)],  # cols
+        [board[i][0] for i in range(3)],  # columns
+        [board[i][1] for i in range(3)],  # columns
+        [board[i][2] for i in range(3)],  # columns
         board[0],  # rows
         board[1],  # rows
         board[2],  # rows
         [board[i][i] for i in range(3)],  # main diagonal
-        [board[i][2 - i] for i in range(3)]  # antisec diagonal
+        [board[i][2 - i] for i in range(3)]  # anti-diagonal
     ]
+
     if [X, X, X] in lines:
         return X
     elif [O, O, O] in lines:
         return O
-    return None
+    else:
+        return None
+
 
 
 
@@ -75,7 +68,7 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    return winner(board) is not None or not any(EMPTY in row for row in board)
+    return winner(board) is not None or EMPTY not in sum(board, [])
 
 
 
@@ -84,6 +77,7 @@ def utility(board):
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
     win = winner(board)
+
     if win == X:
         return 1
     elif win == O:
@@ -100,38 +94,42 @@ def minimax(board):
         return None
 
     if player(board) == X:
-        _, move = max_value(board)
+        _, move = max_value(board, -math.inf, math.inf)
         return move
     else:
-        _, move = min_value(board)
+        _, move = min_value(board, -math.inf, math.inf)
         return move
     
-def max_value(board):
+def max_value(board, alpha, beta):
     if terminal(board):
         return utility(board), None
+
     v = -math.inf
     move = None
     for action in actions(board):
-        min_v, _ = min_value(result(board, action))
+        min_v, _ = min_value(result(board, action), alpha, beta)
         if min_v > v:
             v = min_v
             move = action
-            if v == 1:
-                return v, move
+        alpha = max(alpha, v)
+        if beta <= alpha:
+            break
     return v, move
 
-def min_value(board):
+def min_value(board, alpha, beta):
     if terminal(board):
         return utility(board), None
+
     v = math.inf
     move = None
     for action in actions(board):
-        max_v, _ = max_value(result(board, action))
+        max_v, _ = max_value(result(board, action), alpha, beta)
         if max_v < v:
             v = max_v
             move = action
-            if v == -1:
-                return v, move
+        beta = min(beta, v)
+        if beta <= alpha:
+            break
     return v, move
 
 
